3bc14b35a8df4bc55b1f12e2afa99f968c32852f
Aditya Gupta <adityagupta.nits@gmail.com>
2026-03-30 23:50:59 +0530

    removed import huggingface

--- backend/rag.py
+++ backend/rag.py
@@ -1,6 +1,5 @@
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 import json
