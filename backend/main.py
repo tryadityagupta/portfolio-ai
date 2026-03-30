dc96380b2c27468afac98944af637b16cb39eb5b
Aditya Gupta <adityagupta.nits@gmail.com>
2026-03-30 21:19:23 +0530

    Remove duplicate code from main.py

--- backend/main.py
+++ backend/main.py
@@ -6,14 +6,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # allow Vercel frontend
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 # allow frontend connection
 app.add_middleware(
     CORSMiddleware,
